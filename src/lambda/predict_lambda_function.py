0714a691b7aa749a3f79ba1440a2cf66d267c3e8
aalam <aalam@tableau.com>
2020-05-08 09:08:22 -0700

    updated predict to invoke model

--- src/lambda/predict_lambda_function.py
+++ src/lambda/predict_lambda_function.py
@@ -5,13 +5,57 @@
 
 __author__ = 'aalam'
 
-import random
+import os
 import logging
 import json
 import traceback
+import tldextract
+import boto3
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
+
+# grab environment variables
+ENDPOINT_NAME = os.getenv('ENDPOINT_NAME', 'XGBoostEndpoint-2020-05-08-14-42-46')
+VALID_CHARS = 'abcdefghijklmnopqrstuvwxyz0123456789-_.'
+LOOKUP_TABLE = None
+
+
+def extract_domain(record):
+    domain = record
+    ret = ''
+    try:
+        ext = tldextract.extract(domain)
+        ret = ext.domain
+    except:
+        print(record)
+    return ret
+
+
+def pad(l, content, width):
+    l.extend([content] * (width - len(l)))
+    return l
+
+
+def features(domain):
+    global VALID_CHARS
+    global LOOKUP_TABLE
+    if not LOOKUP_TABLE:
+        LOOKUP_TABLE = dict()
+        idx = 1
+        for c in VALID_CHARS:
+            LOOKUP_TABLE[c] = int(idx)
+            idx += int(1)
+    rvalue = list()
+    if len(domain) <= 63 and ' ' not in domain:
+        for c in domain.lower():
+            rvalue.append(str(LOOKUP_TABLE[c]))
+    else:
+        # print(domain)
+        pass
+
+    rvalue = pad(rvalue, '0', 63)
+    return ','.join(rvalue)
 
 
 def response(msg, statuscode, header={},):
@@ -48,15 +92,22 @@ def handler(event, context):
             if operation in operations:
                 payload = event['queryStringParameters'] if operation == 'GET' else json.loads(event['body'])
                 fqdn = payload.get('fqdn', None)
+                runtime = boto3.client('runtime.sagemaker')
+                domain = extract_domain(fqdn)
+                feature_X = features(domain)
+                # print(feature_X)
+                res = runtime.invoke_endpoint(EndpointName=ENDPOINT_NAME,
+                                              ContentType='text/csv',
+                                              Body=feature_X)
+                # print(response)
+                pred = json.loads(res['Body'].read().decode())
+                print(pred)
+                predicted_label = 'dga' if pred > .5 else 'benign'
                 # call predict endpoint here
-                logger.info(msg=payload)
-                if random.randint(0, 10) % 2 == 0:
-                    payload['dga'] = True
-                else:
-                    payload['dga'] = False
-                logger.info(msg=payload)
-                #print(payload)
-                return response(msg=f"{json.dumps(payload)}", statuscode=200)
+                res_payload = {fqdn: predicted_label}
+                logger.info(msg=res_payload)
+                # print(payload)
+                return response(msg=f"{json.dumps(res_payload)}", statuscode=200)
 
             else:
                 return response(msg=f"ERROR: Operation {operation} is not supported \n.", statuscode=400)
