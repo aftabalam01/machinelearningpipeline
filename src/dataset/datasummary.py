f341bd2df1439ae9333c04dc744eb67d078b0207
aalam <aalam@tableau.com>
2020-05-10 08:58:54 -0700

    generated 1 millions

--- src/dataset/datasummary.py
+++ src/dataset/datasummary.py
@@ -2,11 +2,13 @@ import os
 import pandas as pd
 
 def summary(df):
+    print(f" Before: Number of record {df.shape[0]} and number of columns {df.shape[1]}")
     df.drop_duplicates(keep="first", inplace=True)
     df.columns = ['domainName', 'family']
     df['label'] = 'bad'
     df.to_csv(OUTPUT_FILE, compression="gzip", mode='w',index=False)
     print("summary of data by family")
+    print(f"After: Number of record {df.shape[0]} and number of columns {df.shape[1]}")
     print(df.groupby(['family']).agg(['count']))
 
 
